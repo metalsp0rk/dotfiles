486a28233e9aebfc7ea211809716b9d453b74b20
Kyle Brennan <kbrennan@605.tv>
2022-10-25 13:04:23 -0700

    update harpoon config to include terraform/core macros

--- neovim/.config/nvim/lua/plug-config/harpoon.lua
+++ neovim/.config/nvim/lua/plug-config/harpoon.lua
@@ -10,10 +10,29 @@ require("harpoon").setup({
   tmux_autoclose_windows = false,
   excluded_filetypes = { "harpoon" },
   mark_branch = false,
+  projects = {
+    ["$HOME/605/stack-core"] = {
+      term = {
+        cmds = {
+        }
+      }
+    },
+    ["$HOME/605/stack-terraform"] = {
+      term = {
+        cmds = {
+          "terraform init",
+          "terraform plan --out=.terraform.tfstate",
+          "terraform apply .terraform.tfstate",
+          "terraform init && terraform plan --out=.terraform.tfstate",
+        }
+      }
+    }
+  }
 })
 
 nnoremap("<leader>a", function() require("harpoon.mark").add_file() end)
 nnoremap("<C-e>", function() require("harpoon.ui").toggle_quick_menu() end)
+nnoremap("<leader>hc", function() require("harpoon.cmd-ui").toggle_quick_menu() end)
 
 nnoremap("<C-h>", function() require("harpoon.ui").nav_file(1) end)
 nnoremap("<C-j>", function() require("harpoon.ui").nav_file(2) end)
