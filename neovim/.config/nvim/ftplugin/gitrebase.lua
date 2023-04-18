923a15a852268f700c3193945f380ee232a49d12
Kyle Brennan <kyleb@planetart.com>
2023-04-18 15:50:09 -0700

    fixup it all

---
+++ neovim/.config/nvim/ftplugin/gitrebase.lua
@@ -0,0 +1,14 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+-- fugitive rebase shortcuts
+keymap("n", "I", "0cepick<esc>", opts)
+keymap("n", "R", "0cereword<esc>", opts)
+keymap("n", "E", "0ceedit<esc>", opts)
+keymap("n", "S", "0cesquash<esc>", opts)
+keymap("n", "F", "0cefixup<esc>", opts)
+keymap("n", "A", "0ceexec<esc>", opts)
+keymap("n", "D", "0cedrop<esc>", opts)
+
+
+keymap("n", "<leader>gbf", ":g/fixup/normal 0cefixup<esc>", opts)
+keymap("n", "<leader>gbs", ":g/squash/normal 0cesquash<esc>", opts)
--- neovim/.config/nvim/lua/keyMappings.lua
+++ neovim/.config/nvim/lua/keyMappings.lua
@@ -73,13 +73,3 @@ keymap("v", ">", ">gv", opts)
 keymap("v", "<", "<gv", opts)
 
 
--- fugitive rebase shortcuts
-keymap("n", "I", "0cepick<esc>", opts)
-keymap("n", "R", "0cereword<esc>", opts)
-keymap("n", "E", "0ceedit<esc>", opts)
-keymap("n", "S", "0cesquash<esc>", opts)
-keymap("n", "F", "0cefixup<esc>", opts)
-keymap("n", "A", "0ceexec<esc>", opts)
-keymap("n", "D", "0cedrop<esc>", opts)
-
-
