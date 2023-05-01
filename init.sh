889efac9adff768495528057a5ff3aee825be097
Kyle Brennan <kyleb@planetart.com>
2023-05-01 12:30:25 -0700

    clean up old version in preparation for new makefile-based paradigm.

--- init.sh
+++ init.sh
@@ -7,8 +7,6 @@ chmod +x /tmp/brew-install.sh
 
 echo "Homebrew Installed, gathering packages..."
 
-brew install yq ack jq git fzf zsh helm krew kubernetes-cli awscli make python@3.10 ranger rust terraform-lsp tfenv tmux watch wget xz zstd terraform-ls lua-language-server golang tfenv
+brew install make python@3.10 
 
 echo "done!"
-
-echo ""
