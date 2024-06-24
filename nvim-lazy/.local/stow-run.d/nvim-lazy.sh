a4fe2f27861f65d147cda6445d2a0dff6a3e67b6
Kyle Brennan <kyle@metalspork.xyz>
2024-06-23 17:33:10 -0700

    stow-run for nvim

---
+++ nvim-lazy/.local/stow-run.d/nvim-lazy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+unameout="$(uname -s)"
+case "${unameout}" in
+  Linux*)	machine=Linux;;
+  Darwin*)	machine=Mac;;
+  CYGWIN*)	machine=Cygwin;;
+  MINGW*)	machine=MinGw;;
+  *)		machine="UNKNOWN:${unameout}";;
+esac
+
+if [ "${machine}" == "Linux" ]; then
+  yay -S --needed --noconfirm neovim yarn nodejs rubygems perl npm zip unzip
+fi
+
+if [ "${machine}" == "Mac" ]; then
+brew install neovim terraform-ls lua-language-server golang rust python@3.10 ripgrep python-pynvim fd yarn
+fi
+yarn install -g neovim
+gem install neovim
+cpan install neovim
+
+# install-helm-ls
+
+if [ "${machine}" == "Mac" ]; then
+curl -L https://github.com/mrjosh/helm-ls/releases/download/v0.0.3/helm_ls_darwin_arm64 -o $HOME/.local/bin/helm-ls
+chmod +x $HOME/.local/bin/helm-ls
+fi
+
+if [ "${machine}" == "Linux" ]; then
+  yay -S helm-ls --noconfirm --needed
+fi
+curl -L https://github.com/mrjosh/helm-ls/releases/download/v0.0.3/helm_ls_darwin_arm64 -o $HOME/.local/bin/helm-ls
