da1b4982e2b18852042f9a71873ff08d518cc68a
Kyle Brennan <kyle@metalspork.xyz>
2024-07-08 22:04:20 -0700

    fix up waybar config and add emoji detection with telescope in vim

--- nvim-lazy/.config/nvim/lua/metalspork/plugins/telescope.lua
+++ nvim-lazy/.config/nvim/lua/metalspork/plugins/telescope.lua
@@ -2,12 +2,12 @@ return {
   "nvim-telescope/telescope.nvim",
   version = "*",
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "ghassan0/telescope-glyph.nvim"
+    "nvim-lua/plenary.nvim"
   },
   config = function()
     local builtin = require('telescope.builtin')
-    require('telescope').load_extension('glyph')
+    require('telescope').load_extension('nerdy')
+    -- require('telescope').load_extension('glyph')
   end,
   keys = {
     { mode = {'n'}, "<leader>pf", function() require('telescope.builtin').find_files() end, desc = "Telescope: Project Files"},
@@ -16,7 +16,8 @@ return {
     { mode = {'n'}, "<leader>gb", function() require('telescope.builtin').git_branches() end, desc = "Telescope: Git Branch"},
     { mode = {'n'}, "<C-p>", function() require('telescope.builtin').git_files() end, desc = "Telescope: Git Files"},
     { mode = {'n'}, "<C-o>", function() require('telescope.builtin').buffers() end, desc = "Telescope: Buffers"},
-    { mode = {'n'}, "<leader>fe", "<CMD>Telescope glyph<CR>", desc = "Telescope: Emoji"},
+    -- { mode = {'n'}, "<leader>fe", "<CMD>Telescope glyph<CR>", desc = "Telescope: Emoji"},
+    { mode = {'n'}, "<leader>fe", "<CMD>Telescope nerdy<CR>", desc = "Telescope: Emoji"},
     { mode = {'n'}, "<leader>ps", function() require('telescope.builtin').grep_string({search = vim.fn.input('Grep > ')}) end, desc = "Telescope: Grep"},
   }
 }
