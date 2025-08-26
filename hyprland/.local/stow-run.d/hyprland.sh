2bcd4dd0741a7fffa1cc22f97c29d5952473ff44
Kyle Brennan <kyle@metalspork.xyz>
2025-08-25 18:26:07 -0700

    deploy more configs

--- hyprland/.local/stow-run.d/hyprland.sh
+++ hyprland/.local/stow-run.d/hyprland.sh
@@ -1,1 +1,21 @@
-noto-fonts-emoji
+#!/usr/bin/env bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+Linux*) machine=Linux ;;
+Darwin*) machine=Mac ;;
+CYGWIN*) machine=Cygwin ;;
+MINGW*) machine=MinGw ;;
+*) machine="UNKNOWN:${unameOut}" ;;
+esac
+
+if [ "${machine}" == "Linux" ]; then
+
+  yay --noconfirm --needed -S noto-fonts-emoji waybar wttrbar light wlsunset kanshi wmenu swww
+  yay -S --noconfirm --needed ttf-sourcecodepro-nerd
+  yay -S --noconfirm --needed newaita-icons-git
+  yay -S --noconfirm --needed ttf-md-fonts-git
+  yay -S --noconfirm --needed ttf-font-awesome
+  yay -S --noconfirm --needed awesome-terminal-fonts
+
+fi
