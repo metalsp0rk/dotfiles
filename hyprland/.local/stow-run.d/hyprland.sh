721a01617dedff66c8740e71827ecde3cfb9f0ac
Kyle Brennan <kyle@metalspork.xyz>
2025-10-21 22:29:42 -0700

    sync sync sync

--- hyprland/.local/stow-run.d/hyprland.sh
+++ hyprland/.local/stow-run.d/hyprland.sh
@@ -11,11 +11,13 @@ esac
 
 if [ "${machine}" == "Linux" ]; then
 
-  yay --noconfirm --needed -S waybar wttrbar light wlsunset kanshi wmenu swww walker-bin libqalculate
-  yay --noconfirm --needed -S noto-fonts-emoji ttf-sourcecodepro-nerd newaita-icons-git ttf-md-fonts-git ttf-font-awesome awesome-terminal-fonts ttf-boxicons
+  yay --noconfirm --needed -S waybar wttrbar light wlsunset kanshi wmenu swww libqalculate hypridle hyprlock
+  yay --noconfirm --needed -S noto-fonts-emoji ttf-sourcecodepro-nerd newaita-icons-git ttf-md-fonts-git ttf-font-awesome awesome-terminal-fonts ttf-boxicons swayosd
+  yay --noconfirm --needed -S walker elephant elephant-{desktopapplications,clipboard,calc,files,symbols,unicode,bin}
   hyprpm update
   hyprpm add https://github.com/outfoxxed/hy3
   hyprpm update
   hyprpm enable hy3
+  sudo systemctl enable --now swayosd-libinput-backend
 
 fi
